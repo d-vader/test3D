3d7a49fdc12c25e300c7775244e070e783b3ec43
Illia <gognyak@attractgroup.com>
2018-10-24 21:38:02 +0300

    ignore

---
+++ README.md
@@ -0,0 +1,18 @@
+
+                          Test Task
+
+  What is it?
+  -----------
+
+  This is my test task for new position
+
+  Installation
+  ------------
+
+  Run `npm i`
+  Then `npm run build`
+
+  Contacts
+  --------
+
+     o If you have any comments or suggestions, please contact me `EMAIL@mail.ru`
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,84 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MergeIntoSingleFilePlugin = require('webpack-merge-and-include-globally');
+const ConcatPlugin = require('webpack-concat-plugin');
 
 module.exports = {
     entry: [
-        './src/index.js',
+        './src/js/index.js',
+        './src/styles/style.scss',
+
     ],
     output: {
-        filename: './dist/bundle.js'
+        filename: 'js/bundle.js'
     },
     devtool: "source-map",
     module: {
-        rules: [{
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src'),
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: 'env'
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
                 }
-            }
-        },
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/styles'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            minimize: true,
+                            url: false
+                        }
+                    },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin({
+            filename: './styles/style.bundle.css',
+            allChunks: true,
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        // new MergeIntoSingleFilePlugin({
+        //     "libs.js": [
+        //         path.resolve(__dirname, 'src/libs/three.js'),
+        //         path.resolve(__dirname, 'src/libs/OrbitControls.js'),
+        //         path.resolve(__dirname, 'src/libs/WebGL.js'),
+        //     ]
+        // }),
+        new ConcatPlugin({
+            uglify: true,
+            sourceMap: true,
+            name: 'libs',
+            outputPath: 'libs/',
+            fileName: '[name].[hash:8].js',
+            filesToConcat: ['./src/libs/three.js', './src/libs/OrbitControls.js', './src/libs/WebGL.js'],
+            attributes: {
+                async: false
+            }
+        })
     ]
 };
